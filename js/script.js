008e63e529f5a5fe786c541d582dca38f7fdcfae
akokora <andrey18121996@gmail.com>
2022-07-04 00:14:38 +0300

    countdown

---
+++ js/script.js
@@ -0,0 +1,53 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const deadLine = '2022-11-30';
+
+  function getTimeRemaining(endtime) {
+    const time = Date.parse(endtime) - Date.parse(new Date());
+    const days = Math.floor(time / (1000 * 60 * 60 * 24)); //* Сколько миллесикунд в одном дне
+    const hours = Math.floor((time / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((time / 1000 / 60) % 60);
+    const seconds = Math.floor((time / 1000) % 60);
+
+    return {
+      total: time,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
+  }
+
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector);
+    const days = timer.querySelector('#days');
+    const hours = timer.querySelector('#hours');
+    const minutes = timer.querySelector('#minutes');
+    const seconds = timer.querySelector('#seconds');
+    const timeInterval = setInterval(updateClock, 1000);
+
+    updateClock();
+
+    function updateClock() {
+      const time = getTimeRemaining(endtime);
+
+      days.innerHTML = getZero(time.days);
+      hours.innerHTML = getZero(time.hours);
+      minutes.innerHTML = getZero(time.minutes);
+      seconds.innerHTML = getZero(time.seconds);
+
+      if (time.total <= 0) {
+        clearInterval(setInterval);
+      }
+    }
+  }
+
+  setClock('.timer', deadLine);
+});
